339aec41d2d1c7de5df938c886df5a16e6b5ab0d
builderbob <rmccarth@alumni.cmu.edu>
2024-08-26 16:16:33 -0400

    update deployment instructions

--- configuration.nix
+++ configuration.nix
@@ -163,7 +163,7 @@
 
   system.stateVersion = "24.05"; # Did you read the comment? nope
 
-
+  # sudo chown builderbob:users -R /etc/nixos
   # ln -s /home/nixos/dev/play/learn-nix/configuration.nix /etc/nixos/configuration.nix
 }
 
