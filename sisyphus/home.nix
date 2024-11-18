1cf306fff0620e054a54e42315233d69aba8ddd1
Rob McCarthy <r.mccarthy@wearemetronome.com>
2024-11-18 15:57:02 -0500

    add work machine init

---
+++ sisyphus/home.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, home-manager, ... }:
+{
+  home.username = "rob.mccarthy";
+  home.homeDirectory = "/Users/rob.mccarthy";
+
+  home.stateVersion = "24.11";
+  nixpkgs.config.allowUnfree = true;
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    awscli2
+    bandit
+    cargo
+    cosign
+    crane
+    cypress
+    direnv
+    fluxcd
+    git
+    gnumake
+    go
+    google-chrome 
+    imgpkg
+    iterm2
+    jq
+    k3d
+    kbld
+    kubectl
+    kubernetes-helm
+    nmap
+    nodejs_22
+    openssl
+    podman
+    poetry
+    python311Full
+    rectangle
+    ripgrep
+    rustc
+    semgrep
+    shellcheck-minimal
+    skopeo
+    slack
+    spotify
+    teams
+    terraform
+    tflint
+    tmux
+    vendir
+    vim
+    vscode
+    xcode-install
+    yamllint
+    ytt
+    zarf
+
+    # jiggler not in nixpkgs
+    # mattermost-desktop wrong arch
+  ];
+}
